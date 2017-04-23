4577818850d9578d44e4aba21fe2aa04c67412ee
gewenrui <941137860@qq.com>
2017-04-23 15:38:38 +0800

    confirm

--- authorize.php
+++ authorize.php
@@ -5,32 +5,44 @@
  * Date: 2017/4/21
  * Time: 上午6:36
  */
+
 require_once __DIR__ . '/server.php';
 
 session_start();
 $request = OAuth2\Request::createFromGlobals();
 $response = new OAuth2\Response();
-
-
-if (empty($_SESSION['userId'])) {
-    echo '您未在第三方平台登录';
-    exit('
-<form method="post">
-  <label>输入用户名&密码</label><br />
-  <input type="text" name="authorized" value="yes">
-  <input type="text" name="authorized" value="no">
-   <input type="submit" name="authorized" value="提交">
-</form>');
-
-}
-
-
-$storage->checkUserCredentials();
-// validate the authorize request
+//跨域请求开始
+$origin = isset($_SERVER['HTTP_ORIGIN']) ? $_SERVER['HTTP_ORIGIN'] : '';
+header('Access-Control-Allow-Origin:' . $origin);
+header('Access-Control-Allow-Methods:OPTIONS, GET, POST'); // 允许option，get，post请求
+header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
+header('Access-Control-Allow-Credentials:true');
+header('Access-Control-Max-Age:86400');
+//跨域请求结束
+$request_body = file_get_contents('php://input');
+$res = json_decode($request_body, true);
+//获取用户名和密码
+$email = isset($_POST['params']['email']) ? $_POST['params']['email'] : '';
+$password = isset($_POST['params']['password']) ? $_POST['params']['password'] : '';
+//使用sha1加密密码
+//echo $email.''.$pass
+//check 请求是否合法
 if (!$server->validateAuthorizeRequest($request, $response)) {
     $response->send();
     die;
 }
+$res = $storage->checkUserCredentials($email, $password);
+
+if($res){
+    //查找其user_id
+    $user_id = $storage->getUser($email)['user_id'];
+    $_SESSION['userId'] = $user_id;
+    echo json_encode(array('code' => 0 ,'message' => '成功!'));
+}else{
+    echo json_encode(array('code' => 1 ,'message' => '失败!'));
+}
+// validate the authorize request
+
 //insert into oauth_users(username,password,first_name,last_name) values('gewenrui','40bd001563085fc35165329ea1ff5c5ecbdbbeef','ge','wenrui');
 //如果没有登录状态的话,让其登录
 
