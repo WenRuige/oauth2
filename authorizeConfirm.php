7685f85c9b62495a7b86c95973eb7a2fa10d5e40
gewenrui <941137860@qq.com>
2017-04-23 16:51:43 +0800

    update

--- authorizeConfirm.php
+++ authorizeConfirm.php
@@ -2,61 +2,32 @@
 /**
  * Created by PhpStorm.
  * User: gewenrui
- * Date: 2017/4/21
- * Time: 上午6:36
+ * Date: 2017/4/23
+ * Time: 下午3:47
  */
+
 
 require_once __DIR__ . '/server.php';
 session_start();
-$request = OAuth2\Request::createFromGlobals();
-$response = new OAuth2\Response();
-//跨域请求开始
 $origin = isset($_SERVER['HTTP_ORIGIN']) ? $_SERVER['HTTP_ORIGIN'] : '';
 header('Access-Control-Allow-Origin:' . $origin);
 header('Access-Control-Allow-Methods:OPTIONS, GET, POST'); // 允许option，get，post请求
 header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
 header('Access-Control-Allow-Credentials:true');
 header('Access-Control-Max-Age:86400');
-//跨域请求结束
+var_dump($_SESSION);die;
+$request = OAuth2\Request::createFromGlobals();
+$response = new OAuth2\Response();
+
 
 if (!$server->validateAuthorizeRequest($request, $response)) {
     $response->send();
     die;
 }
-$request_body = file_get_contents('php://input');
-$res = json_decode($request_body, true);
-//获取用户名和密码
-$email = isset($res['params']['email']) ? $res['params']['email'] : '';
-$password = isset($res['params']['password']) ? $res['params']['password'] : '';
-//check 请求是否合法
-
-$res = $storage->checkUserCredentials($email, $password);
-if ($res) {
-    //查找其user_id
-    $user_id = $storage->getUser($email)['user_id'];
-    $_SESSION['userId'] = $user_id;
-    echo json_encode(array('code' => 0, 'message' => '成功!'));
-} else {
-    echo json_encode(array('code' => 1, 'message' => '失败!'));
-}
-// validate the authorize request
-
-//insert into oauth_users(username,password,first_name,last_name) values('gewenrui','40bd001563085fc35165329ea1ff5c5ecbdbbeef','ge','wenrui');
-//如果没有登录状态的话,让其登录
 
 
-die;
 
 
-// display an authorization form
-if (empty($_POST)) {
-    exit('
-<form method="post">
-  <label>是否认证testclient</label><br />
-  <input type="submit" name="authorized" value="yes">
-  <input type="submit" name="authorized" value="no">
-</form>');
-}
 $user_id = 1;
 //curl -u testclient:testpass http://127.0.0.1:8881/token.php -d 'grant_type=authorization_code&code=2ddb4a94b24ac2fd8a368b6707af087a88a7f576'
 // print the authorization code if the user has authorized your client
@@ -68,6 +39,3 @@ $server->handleAuthorizeRequest($request, $response, $is_authorized, $user_id);
 //    exit("SUCCESS! Authorization Code: $code");
 //}
 $response->send();
-
-
-
---
+++ fuck.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: gewenrui
+ * Date: 2017/4/23
+ * Time: 下午4:17
+ */
+
+session_start();
+
+$_SESSION['user_id'] = 123;
